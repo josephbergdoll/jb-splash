d821cc36bc81181039dbdec006a4d5150e884b61
Joseph Bergdoll <j@varsity.is>
2014-05-17 02:40:36 -0400

    swag

--- js/scripts-ck.js
+++ js/scripts-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});$(window).load(function(){$(".mast-bg").delay(600).queue(function(){$(this).addClass("show-bg")});$(".info-contain").delay(900).queue(function(){$(this).addClass("show-info")})});
+$(document).ready(function(){});$(window).load(function(){$(".mast-bg").delay(900).queue(function(){$(this).addClass("show-bg")});$(".info-contain").delay(1500).queue(function(){$(this).addClass("show-info")})});
--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 
 $(window).load(function(){
 
-  $('.mast-bg').delay(600).queue(function(){
+  $('.mast-bg').delay(900).queue(function(){
     $(this).addClass('show-bg');
   });
 
-  $('.info-contain').delay(900).queue(function(){
+  $('.info-contain').delay(1500).queue(function(){
     $(this).addClass('show-info');
   });
 
